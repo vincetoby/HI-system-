be9d8c71cc9ef0535503ed9db7695b18ce807d8a
Vincent Ekechukwu <136026786+vincetoby@users.noreply.github.com>
2024-08-27 21:38:08 +0100

    Update HIS.route.js

--- Health Information System/routes/HIS.route.js
+++ Health Information System/routes/HIS.route.js
@@ -1,9 +1,2 @@
 import HISController from "../controllers/HIS.controller.js";
-import authenticateHospital from "../middleware/authonticationhospital.js";
-import  Router  from "express";
-const router = Router();
-
-router.post("/register", HISController.signup);
- router.post("/login",HISController.loginHospital); //
- 
-export default router;
+import
